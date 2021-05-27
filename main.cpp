f79d0aaac9d9cc8121b7656b4701654b00a051e6
NikitinaD <dariya.nikitina1997@gmail.com>
2021-05-27 15:03:16 +0300

    added student name input

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char name[20];
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!\n";
+	system("pause");
+	return 0;
+}
